3fd9375ea0fac4261d862fdef73280892b804f83
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-06-17 23:04:13 +0530

    Completed, css is remaining

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -12,7 +13,8 @@ import { SalaryComponent } from './salary/salary.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/salary/salary.component.ts
+++ src/app/salary/salary.component.ts
@@ -7,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalaryComponent implements OnInit {
 
-  a = 10;
+  a = 0;
+
+  bs:number = 0;
+  ta:number = 0;
+  da:number = 0;
+  hra:number = 0;
+  cla:number = 0;
+  pf:number = 0;
+  insurance:number = 0;
+  grossSalary:number = 0;
+  basic_salary:number = 0;
+  netSalary:number = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  calculateSalary() {
+  
+  console.log(this.basic_salary);
+  this.bs = this.basic_salary * 1;
+  this.ta = this.basic_salary * 0.13;
+  this.da = this.basic_salary * 0.14;
+  this.hra = this.basic_salary * 0.12;
+  this.cla = this.basic_salary  * 0.11;
+  this.insurance = this.basic_salary  * 0 + 500 ;
+  this.pf = this.basic_salary * 0.15;
+  
+  this.grossSalary = this.ta + this.da + this.hra + this.cla + this.pf + this.bs;
+  this.netSalary = this.grossSalary - this.pf - this.insurance;
   }
 }
