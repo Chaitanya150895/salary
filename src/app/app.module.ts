5ccec2c8b9a24c011433823d0a663b13dec350ea
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-06-22 18:38:32 +0530

    working on bootstrap styling class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SalaryComponent } from './salary/salary.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SalaryComponent
+    SalaryComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
